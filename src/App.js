4e0f3e0ca7d9bd66cef9110d3c0d7122cdcc8f5f
VivianC <viviancao58@gmail.com>
2023-04-15 20:29:33 +1000

    create home route

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 
@@ -5,6 +6,9 @@ function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" exact />
+      </Routes>
     </>
   );
 }
