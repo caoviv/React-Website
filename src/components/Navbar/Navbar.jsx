431ef4636bf69c7fbd43139d1b8bac74e47ae42d
VivianC <viviancao58@gmail.com>
2023-04-15 20:41:20 +1000

    create menu toggle symbol

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 function Navbar() {
+  const [menuToggle, setMenuToggle] = useState(false);
+
+  const handleMenuToggle = () => setMenuToggle(!menuToggle);
+
   return (
     <>
       <nav className="navbar">
@@ -8,6 +13,11 @@ function Navbar() {
           <NavLink to="/" className="navbar-logo">
             Vegemite <i class="fa-solid fa-dog" />
           </NavLink>
+          <div className="menu-icon" onClick={handleMenuToggle}>
+            <i
+              className={menuToggle ? "fa-solid fa-x" : "fa-solid fa-bars"}
+            ></i>
+          </div>
         </div>
       </nav>
     </>
