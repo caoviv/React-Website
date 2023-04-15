d23bb9066b3af0e36fbd623503f7698878f9fc1c
VivianC <viviancao58@gmail.com>
2023-04-15 20:50:53 +1000

    add menu items

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,6 +18,28 @@ function Navbar() {
               className={menuToggle ? "fa-solid fa-x" : "fa-solid fa-bars"}
             ></i>
           </div>
+          <ul className={menuToggle ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <NavLink to="/" className="nav-links" onClick={handleMenuToggle}>
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/services" className="nav-links" onClick={handleMenuToggle}>
+                Services
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/products" className="nav-links" onClick={handleMenuToggle}>
+                Products
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/products" className="nav-links-mobile" onClick={handleMenuToggle}>
+                Sign Up
+              </NavLink>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
