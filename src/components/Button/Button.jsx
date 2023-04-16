73b428eaf19edd5258011db7e97a9bebc3467029
VivianC <viviancao58@gmail.com>
2023-04-16 15:32:46 +1000

    create btn comp w custom styles & colours

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+const STYLES = ["btn--primary", "btn==outline"];
+
+const SIZES = ["btn--medium", "btn--large"];
+
+export const Button = ({
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize,
+}) => {
+  const setButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+
+  const setButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
+  return (
+    <Link to="/signup" className="btn-mobile">
+      <button
+        className={`btn ${setButtonStyle} ${setButtonSize}`}
+        onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
+    </Link>
+  );
+};
