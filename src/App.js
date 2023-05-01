e11423acf3342aca5cce7e9685b84c43839c0b80
VivianC <viviancao58@gmail.com>
2023-05-01 16:03:33 +1000

    add Home page to route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import "./AppStyles.css";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" exact />
+        <Route path="/" exact element={<Home />} />
       </Routes>
     </>
   );
