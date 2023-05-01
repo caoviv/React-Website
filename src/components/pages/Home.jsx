2f4a97c0515c8196091d406ed7e866f077f83376
VivianC <viviancao58@gmail.com>
2023-05-01 16:13:15 +1000

    add Cards comp to Home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,12 @@
 import "../../AppStyles.css";
+import Cards from "../Cards/Cards";
 import HeroSection from "../HeroSection/HeroSection";
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <Cards />
     </>
   );
 }
