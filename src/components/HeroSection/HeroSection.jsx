c4c90f50edf395bff528ead71e6dab1428a980d9
VivianC <viviancao58@gmail.com>
2023-05-03 13:49:04 +1000

    replace placeholder text

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -5,22 +5,22 @@ function HeroSection() {
   return (
     <div className="hero-container">
       <video src="/videos/video-1.mp4" autoPlay loop muted />
-      <h1>ADVENTURE AWAITS</h1>
-      <p>What are you waiting for?</p>
+      <h1>Tagline</h1>
+      <p>question?</p>
       <div className="hero-btns">
         <Button
           className="btns"
           buttonStyle="btn--outline"
           buttonSize="btn--large"
         >
-          GET STARTED
+          ACTION
         </Button>
         <Button
           className="btns"
           buttonStyle="btn--primary"
           buttonSize="btn--large"
         >
-          WATCH TRAILER <i class="fa-solid fa-circle-play" />
+          WATCH VIDEO <i class="fa-solid fa-circle-play" />
         </Button>
       </div>
     </div>
