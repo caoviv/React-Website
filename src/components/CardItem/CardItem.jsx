b377979f4d18f2c105425ee942dd16e4ec5cf93c
VivianC <viviancao58@gmail.com>
2023-05-03 13:46:23 +1000

    add card props

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -1,17 +1,15 @@
 import { NavLink } from "react-router-dom";
 
-function CardItem() {
+function CardItem(props) {
   return (
     <>
       <li className="cards-item">
-        <NavLink className="cards-item-link">
-          <figure className="cards-item-pic-wrap">
-            <img src="/" alt="Travel" className="cards-item-img" />
+        <NavLink className="cards-item-link" to={props.path}>
+          <figure className="cards-item-pic-wrap" data-category={props.label}>
+            <img className="cards-item-img" alt="Travel" src={props.src} />
           </figure>
           <div className="cards-item-info">
-            <h5 className="cards-item-text">
-                
-            </h5>
+            <h5 className="cards-item-text">{props.text}</h5>
           </div>
         </NavLink>
       </li>
