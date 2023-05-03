5729e29a76fac102b0a592d63c9d9de9d6889c89
VivianC <viviancao58@gmail.com>
2023-05-03 13:46:35 +1000

    create cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,14 +1,46 @@
 import CardItem from "../CardItem/CardItem";
+import "./CardsStyles.css";
 
 function Cards() {
   return (
     <div className="cards">
-      <h1>Check out these Epic Destinations!</h1>
+      <h1>Tagline</h1>
       <div className="cards-container">
         <div className="cards-wrapper">
-          <div className="cards-items">
-            <CardItem />
-          </div>
+          <ul className="cards-items">
+            <CardItem
+              src="images/img-9.jpg"
+              text="Tagline"
+              label="Category"
+              path="/services"
+            />
+            <CardItem
+              src="images/img-2.jpg"
+              text="Tagline"
+              label="Category"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards-items">
+            <CardItem
+              src="images/img-3.jpg"
+              text="Tagline"
+              label="Category"
+              path="/services"
+            />
+            <CardItem
+              src="images/img-4.jpg"
+              text="Tagline"
+              label="Category"
+              path="/products"
+            />
+            <CardItem
+              src="images/img-8.jpg"
+              text="Tagline"
+              label="Category"
+              path="/sign-up"
+            />
+          </ul>
         </div>
       </div>
     </div>
