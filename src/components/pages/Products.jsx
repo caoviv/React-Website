d57d3346770c9fd3c7d65965d51824fde15c7f65
VivianC <viviancao58@gmail.com>
2023-05-16 14:35:19 +1000

    Create pages

---
+++ src/components/pages/Products.jsx
@@ -0,0 +1,5 @@
+import "../../AppStyles.css";
+
+export default function Products() {
+  return <h1 className="products">PRODUCTS</h1>;
+}
---
+++ src/components/pages/Services.jsx
@@ -0,0 +1,5 @@
+import "../../AppStyles.css";
+
+export default function Services() {
+  return <h1 className="services">SERVICES</h1>;
+}
