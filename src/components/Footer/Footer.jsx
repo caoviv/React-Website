d3b6446fabe2e263cfc5f0ef528930f9fa21b1b1
VivianC <viviancao58@gmail.com>
2023-05-16 14:56:42 +1000

    Create basic Footer layout

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+import { Button } from "../Button/Button";
+import "./FooterStyles.css";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
+          Join the adventure newsletter to receive our best vacation deals
+        </p>
+        <p className="footer-subscription-text">
+          You can unsubscribe at any time.
+        </p>
+        <div className="input-areas">
+          <form>
+            <input
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              className="footer-input"
+            />
+            <Button buttonStyle="btn--outline">Subscribe</Button>
+          </form>
+        </div>
+      </section>
+
+    </div>
+  );
+}
+
+export default Footer;
