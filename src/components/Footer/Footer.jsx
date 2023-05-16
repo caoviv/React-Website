a2edd850e541a873c16e0795c6a8ed6d90cfb0b6
VivianC <viviancao58@gmail.com>
2023-05-16 15:09:02 +1000

    Add Footer details

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import { Button } from "../Button/Button";
+import { NavLink } from "react-router-dom";
 import "./FooterStyles.css";
 
 function Footer() {
@@ -23,7 +24,41 @@ function Footer() {
           </form>
         </div>
       </section>
-
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <NavLink to="/sign-up">How it works</NavLink>
+            <NavLink to="/">Testimonials</NavLink>
+            <NavLink to="/">Careers</NavLink>
+            <NavLink to="/">Investors</NavLink>
+            <NavLink to="/">Terms of Service</NavLink>
+          </div>
+          <div className="footer-link-items">
+            <h2>Contact Us</h2>
+            <NavLink to="/">Contact</NavLink>
+            <NavLink to="/">Support</NavLink>
+            <NavLink to="/">Destinations</NavLink>
+            <NavLink to="/">Sponsorships</NavLink>
+          </div>
+        </div>
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>Videos</h2>
+            <NavLink to="/">Submit Video</NavLink>
+            <NavLink to="/">Ambassadors</NavLink>
+            <NavLink to="/">Agency</NavLink>
+            <NavLink to="/">Influencer</NavLink>
+          </div>
+          <div className="footer-link-items">
+            <h2>Social Media</h2>
+            <NavLink to="/">Instagram</NavLink>
+            <NavLink to="/">Facebook</NavLink>
+            <NavLink to="/">Youtube</NavLink>
+            <NavLink to="/">Twitter</NavLink>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
